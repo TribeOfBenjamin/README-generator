3419674033368763b1e399d135081c8a5c2fbf0c
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-05-30 16:03:43 -0500

    added inquirer promts and README function

---
+++ index.js
@@ -0,0 +1,85 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "title",
+            message: ""
+        },
+        {
+            type: "input",
+            name: "description",
+            message: ""
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: ""
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: ""
+        },
+        {
+            type: "input",
+            name: "license",
+            message: ""
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: ""
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: ""
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: ""
+        },
+    ])
+    .then(function(answers) {
+
+        return console.log(`
+        ![GitHub followers](https://img.shields.io/github/followers/0?label=Follow&style=social)
+        
+        # ${answers.title}
+        ${answers.description}
+        
+        ## Table of Contents
+        
+        - Installation
+        - Usage
+        - License
+        - Contributing
+        - Tests
+        - Questions
+            
+        ## Installation
+        ${answers.installation}
+
+        ## Usage
+        ${answers.usage}
+
+        ## License
+        ${answers.license}
+
+        ## Contributing
+        ${answers.contributing}
+
+        ## Tests
+        ${answers.tests}
+        
+        ## Questions
+        ${answers.questions}
+            
+            
+            
+            `);
+    });
